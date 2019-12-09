42180483d4024f350b224a4c8faa21378d8a7c7f
amySchwingeDM <54595377+amySchwingeDM@users.noreply.github.com>
2019-12-08 22:38:09 -0500

    Add files via upload

---
+++ gameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameController : MonoBehaviour
+{
+    public AudioClip musicClipOne;
+    public AudioClip soundClipOne;
+    public AudioSource musicSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /*musicSource.clip = soundClipOne;
+    musicSource.loop = false;
+    musicSource.Play();*/
+}
